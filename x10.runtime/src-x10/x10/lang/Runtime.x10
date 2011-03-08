scalegraph/sx10

commit db97ac6083419e80df3b1378308a27b61a153b8e

got rid of runtime PlaceLocalHandle replaced with @PerProcess statics
git-svn-id: https://x10.svn.sourceforge.net/svnroot/x10/trunk@20454 a326200e-df2c-42e0-81fa-597e909af41d

x10.runtime/src-x10/x10/lang/Runtime.x10
@@ -338,8 +338,6 @@ import x10.util.NoSuchElementException;
         //Worker Id for CollectingFinish
         val workerId:Int;
 
-        var pool:Pool;
-        
         //1:1 mapping WorkStealing Worker and X10 Worker
         var wsWorker:Object = null;
 
@@ -449,8 +447,8 @@ import x10.util.NoSuchElementException;
     }
 
     @Pinned static class Pool {
-        val latch:SimpleLatch;
-
+        val latch = new SimpleLatch();
+        
         private var size:Int; // the number of workers in the pool
 
         private var spares:Int = 0; // the number of spare workers in the pool
@@ -462,30 +460,15 @@ import x10.util.NoSuchElementException;
         private val semaphore = new Semaphore(0);
 
         // the workers in the pool
-        private val workers:Array[Worker](1){rail};
+        private val workers = new Array[Worker](MAX_THREADS);
 
-        def this(size:Int) {
+        operator this(size:Int):void {
             this.size = size;
-            this.latch = new SimpleLatch();
-            val workers = new Array[Worker](MAX_THREADS);
-
-            // main worker
             workers(0) = worker();
-
-            // other workers
             for (var i:Int = 1; i<size; i++) {
                 workers(i) = new Worker(i);
-            }
-            this.workers = workers;
-        }
-
-        operator this():void {
-            val s = size;
-            for (var i:Int = 1; i<s; i++) {
-                workers(i).pool = this;
                 workers(i).start();
             }
-            workers(0).pool = this;
             workers(0)();
             while (size > dead) Worker.park();
         }
@@ -507,7 +490,6 @@ import x10.util.NoSuchElementException;
                     System.exit(1);
                 }
                 val worker = new Worker(i);
-                worker.pool = this;
                 workers(i) = worker;
                 worker.start();
             }
@@ -555,21 +537,9 @@ import x10.util.NoSuchElementException;
         def size() = size;
     }
 
-    // runtime instance associated with each place
-    public static runtime = PlaceLocalHandle[Runtime]();
-
-    // instance fields
-    transient val pool:Pool;
-    private transient val atomicMonitor:Monitor;
-    public transient val finishStates:FinishState.FinishStates;
-
-    // constructor
-
-    private def this(pool:Pool):Runtime {
-        this.pool = pool;
-        this.atomicMonitor = new Monitor();
-        this.finishStates = new FinishState.FinishStates();
-    }
+    @PerProcess static pool = new Pool();
+    @PerProcess static atomicMonitor = new Monitor();
+    @PerProcess static finishStates = new FinishState.FinishStates();
 
     /**
      * Return the current worker
@@ -585,7 +555,7 @@ import x10.util.NoSuchElementException;
      * Return the number of workers currently in the pool
      * (can increase, cannot decrease)
      */
-    public static def poolSize():Int = runtime().pool.size();
+    public static def poolSize():Int = pool.size();
 
     /**
      * Return the current activity
@@ -619,10 +589,7 @@ import x10.util.NoSuchElementException;
     public static def start(init:()=>void, body:()=>void):void {
         try {
             // initialize thread pool for the current process
-            val pool = new Pool(NTHREADS);
-
             // initialize runtime
-            runtime.set(new Runtime(pool));
             x10rt_registration_complete();
 
             if (hereInt() == 0) {
@@ -632,11 +599,11 @@ import x10.util.NoSuchElementException;
 
                 // wait for thread pool to die
                 // (happens when main activity terminates)
-                pool();
+                pool(NTHREADS);
 
                 // root finish has terminated, kill remote processes if any
                 for (var i:Int=1; i<Place.MAX_PLACES; i++) {
-                    runClosureAt(i, ()=> @x10.compiler.RemoteInvocation {runtime().pool.latch.release();});
+                    runClosureAt(i, ()=> @x10.compiler.RemoteInvocation {pool.latch.release();});
                 }
 
                 // we need to call waitForFinish here to see the exceptions thrown by main if any
@@ -644,7 +611,7 @@ import x10.util.NoSuchElementException;
             } else {
                 // wait for thread pool to die
                 // (happens when a kill signal is received from place 0)
-                pool();
+                pool(NTHREADS);
             }
         } finally {
             if (PRINT_STATS) {
@@ -884,7 +851,7 @@ import x10.util.NoSuchElementException;
     // atomic and when
 
     public static def enterAtomic() {
-        runtime().atomicMonitor.lock();
+        atomicMonitor.lock();
         val a = activity();
         if (a != null)
            a.pushAtomic();
@@ -900,11 +867,11 @@ import x10.util.NoSuchElementException;
         val a = activity();
         if (a != null)
            a.popAtomic();
-        runtime().atomicMonitor.release();
+        atomicMonitor.release();
     }
 
     public static def awaitAtomic():void {
-        runtime().atomicMonitor.await();
+        atomicMonitor.await();
     }
 
     // clocks
@@ -1009,14 +976,14 @@ import x10.util.NoSuchElementException;
     // notify the pool a worker is about to execute a blocking operation
     static def increaseParallelism():void {
         if (!STATIC_THREADS) {
-            runtime().pool.increase();
+            pool.increase();
         }
     }
 
     // notify the pool a worker resumed execution after a blocking operation
     static def decreaseParallelism(n:Int) {
         if (!STATIC_THREADS) {
-            runtime().pool.decrease(n);
+            pool.decrease(n);
         }
     }
 }

x10.runtime/src-cpp/x10/util/IndexedMemoryChunk.cc
@@ -37,7 +37,6 @@ namespace x10 {
         
         void IMC_notifyEnclosingFinish(deserialization_buffer& buf) {
             ref<x10::lang::FinishState> fs = buf.read<ref<x10::lang::FinishState> >();
-            ref<Runtime> rt = PlaceLocalHandle_methods<ref<Runtime> >::__apply(Runtime::FMGL(runtime));
             // olivier says the incr should be just after the notifySubActivitySpawn
             fs->notifyActivityCreation();
             fs->notifyActivityTermination();
@@ -46,8 +45,7 @@ namespace x10 {
         void IMC_serialize_finish_state(place dst, serialization_buffer &buf) {
             // dst is the place where the finish update will occur, i.e. where the notifier runs
             dst = parent(dst);
-            ref<Runtime> rt = PlaceLocalHandle_methods<ref<Runtime> >::__apply(Runtime::FMGL(runtime));
-            ref<x10::lang::FinishState> fs = rt->activity()->finishState();
+            ref<x10::lang::FinishState> fs = Runtime::activity()->finishState();
             fs->notifySubActivitySpawn(Place_methods::_make(dst));
             buf.write(fs);
         }

x10.runtime/src-cpp/x10aux/bootstrap.cc
@@ -41,7 +41,6 @@ x10aux::itable_entry BootStrapClosure::_itables[2] = {
 void x10aux::initialize_xrx() {
     x10::lang::Place_methods::FMGL(places__do_init)();
     x10::lang::Place_methods::FMGL(FIRST_PLACE__do_init)();
-    x10::lang::Runtime::FMGL(runtime__do_init)();
     x10::io::Console::FMGL(OUT__do_init)();
     x10::io::Console::FMGL(ERR__do_init)();
     x10::io::Console::FMGL(IN__do_init)();

x10.runtime/src-cpp/x10aux/network.cc
@@ -451,8 +451,7 @@ void x10aux::cuda_put (place gpu, x10_ulong addr, void *var, size_t sz)
 // teams
 
 void *x10aux::coll_enter() {
-    x10aux::ref<x10::lang::Runtime> rt = x10::lang::PlaceLocalHandle_methods<x10aux::ref<x10::lang::Runtime> >::__apply(x10::lang::Runtime::FMGL(runtime));
-    x10aux::ref<x10::lang::FinishState> fs = rt->activity()->finishState();
+    x10aux::ref<x10::lang::FinishState> fs = Runtime::activity()->finishState();
     fs->notifySubActivitySpawn(x10::lang::Place_methods::_make(x10aux::here));
     fs->notifyActivityCreation();
     return fs._val;

x10.runtime/src-x10/x10/lang/FinishState.x10
@@ -326,7 +326,7 @@ abstract class FinishState {
                 me = (ref as GlobalRef[FinishState]{home==here})();
             } else {
                 val _ref = ref;
-                me = Runtime.runtime().finishStates(ref, ()=>new RemoteFinish(_ref));
+                me = Runtime.finishStates(ref, ()=>new RemoteFinish(_ref));
             }
         }
     }
@@ -390,7 +390,7 @@ abstract class FinishState {
             latch.await();
             if (null != counts) {
                 val root = ref();
-                val closure = ()=>@RemoteInvocation { Runtime.runtime().finishStates.remove(root); };
+                val closure = ()=>@RemoteInvocation { Runtime.finishStates.remove(root); };
                 seen(Runtime.hereInt()) = false;
                 for(var i:Int=0; i<Place.MAX_PLACES; i++) {
                     if (seen(i)) Runtime.runClosureAt(i, closure);
@@ -591,7 +591,7 @@ abstract class FinishState {
                 me = (ref as GlobalRef[FinishState]{home==here})();
             } else {
                 val _ref = ref;
-                me = Runtime.runtime().finishStates(ref, ()=>new RemoteCollectingFinish[T](_ref, tmpReducer));
+                me = Runtime.finishStates(ref, ()=>new RemoteCollectingFinish[T](_ref, tmpReducer));
             }
         }
         public def serialize():SerialData = new SerialData(reducer, super.serialize());

commit 3a2521d2613e1d303acc96be7300010959ed29e7

fixed regression introduced in r20454 leading to non-deterministic crashes when app terminates (confusion between field size and local size)
added additional test to avoid probing after main activity has completed


git-svn-id: https://x10.svn.sourceforge.net/svnroot/x10/trunk@20565 a326200e-df2c-42e0-81fa-597e909af41d

x10.runtime/src-x10/x10/lang/Runtime.x10
@@ -462,10 +462,10 @@ import x10.util.NoSuchElementException;
         // the workers in the pool
         private val workers = new Array[Worker](MAX_THREADS);
 
-        operator this(size:Int):void {
-            this.size = size;
+        operator this(n:Int):void {
+            size = n;
             workers(0) = worker();
-            for (var i:Int = 1; i<size; i++) {
+            for (var i:Int = 1; i<n; i++) {
                 workers(i) = new Worker(i);
                 workers(i).start();
             }
@@ -522,6 +522,7 @@ import x10.util.NoSuchElementException;
                 if (null != activity || latch()) return activity;
                 // go to sleep if too many threads are running
                 semaphore.yield(worker);
+                if (null != activity || latch()) return activity;
                 // try network
                 event_probe();
                 activity = worker.poll();