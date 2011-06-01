scalegraph/sx10

commit fbcdaac64e6d4a2f0780202b41db9bc47d071d5e

Fix XTENLANG-2773
git-svn-id: https://x10.svn.sourceforge.net/svnroot/x10/trunk@22053 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/emitter/Emitter.java
@@ -2632,8 +2632,8 @@ public class Emitter {
 	            && (!isBoxedType(expectedBase))) {
 	        prettyPrint(e, tr);
 	    }
-	    // for primitive
-	    else if (actual.isBoolean() || actual.isNumeric() || actual.isByte()) {
+	    // for primitive and string
+	    else if (actual.isBoolean() || actual.isNumeric() || actual.isByte() || X10PrettyPrinterVisitor.isString(actual, tr.context())) {
 	        if (actual.typeEquals(expectedBase, tr.context())) {
 	            if (e instanceof X10Call && Types.baseType(((X10Call) e).methodInstance().def().returnType().get()) instanceof ParameterType) {
 	                expander = expander.castTo(expectedBase);

commit 35937eb7dab1850bd9277f2e8b81a526eab1131e

Fix regression of Bounds1 in x10c-opt which was introduced in r22053
git-svn-id: https://x10.svn.sourceforge.net/svnroot/x10/trunk@22059 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/x10/emitter/Emitter.java
@@ -2632,8 +2632,8 @@ public class Emitter {
 	            && (!isBoxedType(expectedBase))) {
 	        prettyPrint(e, tr);
 	    }
-	    // for primitive and string
-	    else if (actual.isBoolean() || actual.isNumeric() || actual.isByte() || X10PrettyPrinterVisitor.isString(actual, tr.context())) {
+	    // for primitive
+	    else if (actual.isBoolean() || actual.isNumeric() || actual.isByte()) {
 	        if (actual.typeEquals(expectedBase, tr.context())) {
 	            if (e instanceof X10Call && Types.baseType(((X10Call) e).methodInstance().def().returnType().get()) instanceof ParameterType) {
 	                expander = expander.castTo(expectedBase);
@@ -2665,6 +2665,11 @@ public class Emitter {
 	    else {
 	        if (actual.typeEquals(expected, tr.context()) && !(expected instanceof ConstrainedType) && !(expectedBase instanceof ParameterType) && !(actual instanceof ParameterType)) {
 	            prettyPrint(e, tr);
+	        }
+	        else if (!(actual instanceof ParameterType) && X10PrettyPrinterVisitor.isString(actual, tr.context()) &&
+	        		!(expectedBase instanceof ParameterType) && !X10PrettyPrinterVisitor.isString(expectedBase, tr.context())) {
+	        	expander = expander.boxTo(actual).castTo(expectedBase);
+	        	expander.expand(tr);
 	        }
 	        else {
 	            //cast eagerly