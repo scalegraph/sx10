scalegraph/sx10

commit 9593f5a8b44d839548a16ba802759117b26bb9aa

Fix some bugs in Java.array type initialization. Fix a bug in loading Java inner classes. More complete parsing of generic signatures in the Java classfiles. Fix a silly bug in parsing generic bounds. Make sure a class's name can be obtained without resolving the def. Fix the directory structure of the hadoop samples to correspond to the package structure.
git-svn-id: https://x10.svn.sourceforge.net/svnroot/x10/branches/java-interop@22595 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/polyglot/types/ParsedClassType_c.java
@@ -84,10 +84,20 @@ public abstract class ParsedClassType_c extends ClassType_c implements ParsedCla
         return outer.asType();
     }
 
+    private Name name;
     public Name name() {
-        return def().name();
+        if (name == null) {
+            name = def().name();
+        }
+        return name;
     }
 
+    public ClassType name(Name name) {
+        ParsedClassType_c t = (ParsedClassType_c) copy();
+        t.name = name;
+        return t;
+    }
+    
     /** Get the class's super type. */
     public Type superClass() {
         return Types.get(def().superType());

x10.compiler/src/polyglot/types/ClassType.java
@@ -20,6 +20,7 @@ public interface ClassType extends Importable, ObjectType, MemberInstance<X10Cla
 {
     ClassType flags(Flags flags);
     ClassType container(ContainerType container);
+    ClassType name(Name name);
     
     /**
      * A resolver to access member classes of the class.

x10.compiler/src/polyglot/types/TypeSystem_c.java
@@ -2904,15 +2904,16 @@ public class TypeSystem_c implements TypeSystem
                 }
             };
             cd.name(Name.make("array"));
-            X10ClassDef xiJdef = JavaInterop().def();
-            cd.setPackage(xiJdef.package_());
             cd.position(Position.COMPILER_GENERATED);
-            cd.outer(Types.ref(xiJdef));
-            cd.kind(ClassDef.MEMBER);
             cd.flags(Flags.PUBLIC.Static());
             cd.superType(null);
+            javaArrayType_ = cd.asType(); // Careful here -- JavaInterop will refer back to array
+            X10ClassDef xiJdef = JavaInterop().def();
+            cd.setPackage(xiJdef.package_());
+            cd.outer(Types.ref(xiJdef));
+            cd.kind(ClassDef.MEMBER);
+            xiJdef.addMemberClass(Types.ref(javaArrayType_));
             //cd.setFromJavaClassFile(); // pretend it's a Java class
-            javaArrayType_ = cd.asType();
         }
         return javaArrayType_;
     }

x10.compiler/src/polyglot/types/reflect/ClassFileLazyClassInitializer.java
@@ -134,9 +134,6 @@ public class ClassFileLazyClassInitializer {
 
         if (innerClassInfo != null) {
             outerName = clazz.classNameCP(innerClassInfo.outerClassIndex);
-            if (!packageName.equals("")) {
-                outerName = packageName+"."+outerName;
-            }
             innerName = clazz.className(innerClassInfo.nameIndex);
             // Lazily load the outer class.
             ct.outer(defForName(outerName));
@@ -303,14 +300,23 @@ public class ClassFileLazyClassInitializer {
                 break;
             }
             case 'L': {
-                int start = ++i;
                 Ref<? extends Type> t = null;
+                String parent = null;
+                do {
+                int start = ++i;
                 while (i < str.length()) {
                     char c = str.charAt(i);
                     if (c == ';' || c == '<') {
                         String s = str.substring(start, i);
                         s = s.replace('/', '.');
-                        t = typeForName(s);
+                        if (t == null) {
+                            t = typeForName(s);
+                            parent = s;
+                        } else {
+                            String fullName = parent + "$" + s;
+                            parent = fullName;
+                            t = typeForName(fullName); // FIXME: forgets generic info on container -- should look up member class instead
+                        }
                         break;
                     }
                     i++;
@@ -322,7 +328,6 @@ public class ClassFileLazyClassInitializer {
                     i = updateTypeListFromString(targs, str, i, bounds);
                     assert (i < str.length() && str.charAt(i) == '>');
                     i++;
-                    assert (i < str.length() && str.charAt(i) == ';');
                     X10ClassType cType = t.get().toClass();
                     List<Type> typeArgs = new ArrayList<Type>(targs.size());
                     for (Ref<? extends Type> a : targs) {
@@ -330,6 +335,8 @@ public class ClassFileLazyClassInitializer {
                     }
                     t = Types.ref(cType.typeArguments(typeArgs));
                 }
+                } while (str.charAt(i) == '.'); // Some signatures have generic info for containers
+                assert (i < str.length() && str.charAt(i) == ';');
                 types.add(arrayOf(t, dims));
                 break;
             }
@@ -429,7 +436,8 @@ public class ClassFileLazyClassInitializer {
             name = unboxed;
         }
         name = name.replace('$', '.');
-        return Types.<Type>ref(ts.createClassType(position(), defForName(name, flags)));
+        Name shortName = Name.make(name.substring(name.lastIndexOf('.')+1));
+        return Types.<Type>ref(ts.createClassType(position(), defForName(name, flags)).name(shortName));
     }
 
     public void initTypeObject() {
@@ -618,14 +626,15 @@ public class ClassFileLazyClassInitializer {
         if (str.charAt(0) != '<') return 0;
         int i = 1;
         while (i < str.length()) {
+            if (str.charAt(i) == '>') return i+1;
             int start = i;
             int colon = str.indexOf(':', start);
             String name = str.substring(start, colon);
+            while (str.charAt(colon+1) == ':') colon++; // Sometimes the separator is two colons
             List<Ref<? extends Type>> types = new ArrayList<Ref<? extends Type>>(1);
-            i = updateTypeListFromString(types, str, colon+1, bounds);
+            i = updateTypeFromString(types, str, colon+1, bounds);
             assert (types.size() == 1);
             bounds.put(name, types.get(0));
-            if (str.charAt(i) == '>') return i+1;
             i++;
         }
         assert (str.charAt(i) == '>');

x10.dist/samples/java.interop/hadoop/Makefile
@@ -8,9 +8,9 @@ CLASSPATH = $(shell cat .download/classpath)
 	ant prepare
 
 compile:
-	$(X10C) -classpath "$(CLASSPATH)" *.x10
+	$(X10C) -classpath "$(CLASSPATH)" x10/hadoop/*.x10
 
 run:
-	$(X10) -classpath "$(CLASSPATH)" x10.hadoop.WordCount WordCount.x10 output
+	$(X10) -classpath "$(CLASSPATH)" x10.hadoop.WordCount x10/hadoop/WordCount.x10 output
 	
 

commit 03926f60998c090119197f3737182e37b0ca2989

Fix class name bug introduced in r22595.
git-svn-id: https://x10.svn.sourceforge.net/svnroot/x10/branches/java-interop@22597 a326200e-df2c-42e0-81fa-597e909af41d

x10.compiler/src/polyglot/types/ParsedClassType_c.java
@@ -86,7 +86,7 @@ public abstract class ParsedClassType_c extends ClassType_c implements ParsedCla
 
     private Name name;
     public Name name() {
-        if (name == null) {
+        if (name == null || def.known()) {
             name = def().name();
         }
         return name;