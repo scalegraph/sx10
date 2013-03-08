0083321ae08e30954e768f2ed8ca599ee56faae1
na4zagin3 <mrty.ityt.pt@gmail.com>
2013-03-09 00:00:55 +0900

    add x10rt_team_members to x10rt_front.cc

--- x10.runtime/x10rt/common/x10rt_front.cc
+++ x10.runtime/x10rt/common/x10rt_front.cc
@@ -169,6 +169,11 @@ void x10rt_finalize (void)
 { x10rt_lgl_finalize(); }
 
 
+void x10rt_team_members (x10rt_team team, x10rt_place *members, x10rt_completion_handler *ch, void *arg)
+{
+    x10rt_lgl_team_members(team, members, ch, arg);
+}
+
 
 void x10rt_team_new (x10rt_place placec, x10rt_place *placev,
                      x10rt_completion_handler2 *ch, void *arg)
